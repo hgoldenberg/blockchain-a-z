14841338366e6740777b6cc6f7fb2cfbe6d745bf
Hernan Goldenberg <hgoldenberg@hotmail.com>
2021-10-17 17:21:25 -0300

    Crear un nuevo bloque de la cadena

--- Modulo1-blockchain/blockchain.py
+++ Modulo1-blockchain/blockchain.py
@@ -6,4 +6,33 @@ Created on Sun Oct 17 15:13:31 2021
 @author: hernangoldenberg
 """
 # Modulo 1 - Crear una blockchain
- 
+# Flask==0.12.2 : pip install Flask==0.12.2
+# Cliente HTTP Postman: hhtps://www.getpostman.com/
+
+# Importar las librerias
+
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
+
+
+# Parte 1 - Crear la Blockchain
+
+class Blockchain:
+    
+    def __init__(self):
+        
+        self.chain = []
+        self.create_block (proof = 1, previous_hash = '0')
+        
+    def create_block(self, proof, previous_hash):
+        block = {'index': len(self.chain) + 1,
+                 'timestamp': str(datetime.datetime.now()),
+                 'proof': proof,
+                 'previous_hash': previous_hash}
+        self.chain.append(block)
+        return block
+        
+
+# Parte 2 - Minado de un Blockchain
