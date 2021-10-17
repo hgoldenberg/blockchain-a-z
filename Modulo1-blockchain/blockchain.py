875d39931c75a1f40327f04307f6aeca25142a28
Hernan Goldenberg <hgoldenberg@hotmail.com>
2021-10-17 18:31:43 -0300

    Proof of Work

--- Modulo1-blockchain/blockchain.py
+++ Modulo1-blockchain/blockchain.py
@@ -36,6 +36,19 @@ class Blockchain:
     
     def get_new_block(self):
         return self.chain[-1]
+    
+    def proof_of_work(self, previous_proof):
+        new_proof = 1
+        check_proof = False
+        while check_proof is False:
+            hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
+            if hash_operation [:4] == '0000':
+                check_proof = True
+            else:
+                new_proof += 1
+        return new_proof
+    
+                
         
 
 # Parte 2 - Minado de un Blockchain
