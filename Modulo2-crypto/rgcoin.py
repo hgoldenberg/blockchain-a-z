661a244b8c44c179813cd1e02e4ac17ff05b34c7
Hernan Goldenberg <hgoldenberg@hotmail.com>
2022-03-26 17:36:56 -0300

    Update rgcoin.py
    Descentralizar cadena de nodos

--- Modulo2-crypto/rgcoin.py
+++ Modulo2-crypto/rgcoin.py
@@ -166,7 +166,31 @@ def add_transaction():
     return jsonify(response), 201
 
 # Parte 3 - Descentralizar Cadena de Bloques
+    
+# Conectar nuevos nodos
+@app.route('/connect_node', methods = ['POST'])
+def connect_node():
+    json = request.get_json()
+    nodes = json.get('nodes')
+    if nodes is None: 
+        return 'No hay nodos para añadir', 400
+    for node in nodes:
+        blockchain.add_node(node)
+    response = {'message' : 'Todos los nodos han sido conectados. La cadena de Jbcoins contiene ahora los nodos siguientes: ',
+                'total_nodes': list(blockchain.nodes)}
+    return jsonify(response), 201
 
+# Reemplazar la cadena por la más larga (si es necesario)
+@app.route('/replace_chain', methods = ['GET'])
+def replace_chain():
+    is_chain_replaced = blockchain.replace_chain()
+    if is_chain_replaced:
+        response = {'message' : 'Los nodos tenían diferentes cadenas, que han sido todas reemplazadas por la más larga.',
+                    'new_chain': blockchain.chain}
+    else:
+        response = {'message' : 'Todo correcto. La cadena en todos los nodos ya es la más larga.',
+                    'actual_chain' : blockchain.chain}
+    return jsonify(response), 200  
 
 
 # Ejecutar la app
